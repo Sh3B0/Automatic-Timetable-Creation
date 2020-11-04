4fb6a412e365d3da18f797f7bd1e47ca6a97b2ee
dademd <62954763+dademd@users.noreply.github.com>
2020-11-05 02:50:09 +0300

    Add files via upload

---
+++ Processing/__init__.py
@@ -0,0 +1,9 @@
+# from Processing.schedule import schedule
+
+from Processing.make_sample_table.logic.visualization import Visualization
+import runpy
+from Processing.make_sample_table.logic.parser_for_current_table.parser import parse
+
+if __name__ == '__main__':
+    # parse()
+    vis = Visualization()
