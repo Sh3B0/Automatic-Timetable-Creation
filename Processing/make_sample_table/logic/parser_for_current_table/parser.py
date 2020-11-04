83028b590dca3aed9b65d3fcc2bb3fded1157861
dademd <62954763+dademd@users.noreply.github.com>
2020-11-05 02:20:37 +0300

    Add files via upload

---
+++ Processing/make_sample_table/logic/parser_for_current_table/parser.py
@@ -0,0 +1,69 @@
+import os
+
+import pandas as pd
+from openpyxl import load_workbook
+
+# used for easily accessing files by relative paths while importing this parser
+from pathlib import Path
+
+
+def parse():
+    # first, use "merge and fill.vba"
+    # to merge and fill cells in the table
+    # you get "table vba.xlsm"
+
+    #  load workbook by absolute path to table.
+    #  (Path(__file__).parent gives current working directory
+
+    wb = load_workbook((Path(__file__).parent / 'table merged.xlsx').resolve())
+    sheet = wb.active
+
+    # a week has this much days
+    week = 6
+
+    index = ['9:00-10:30', '10:40-12:10', '12:40-14:10', '14:20-15:50',
+             '16:00-17:30', '17:40-19:10', '19:20-20:50']
+
+    columns = ['Monday', 'Teacher1', 'Room1',
+               'Tuesday', 'Teacher2', 'Room2',
+               'Wednesday', 'Teacher3', 'Room3',
+               'Thursday', 'Teacher4', 'Room4',
+               'Friday', 'Teacher5', 'Room5',
+               'Saturday', 'Teacher6', 'Room6']
+
+    for group_column in range(2, 30 + 1):
+
+        df = pd.DataFrame(columns=columns, index=index)
+        df.columns.name = 'Time Slots'
+
+        # this much days in a week
+        for day in range(week):
+
+            day_row = 3 + day * 22
+            day_column = day * 3
+            for time_slot in range(7):
+                time_slot_row = day_row + 1 + time_slot * 3
+
+                # course name
+                df.iloc[time_slot, day_column] = sheet.cell(row=time_slot_row, column=group_column).value
+                # teacher
+                df.iloc[time_slot, day_column + 1] = sheet.cell(row=time_slot_row + 1, column=group_column).value
+                # room
+                df.iloc[time_slot, day_column + 2] = sheet.cell(row=time_slot_row + 2, column=group_column).value
+
+        # used for printing full dataframe
+        # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        # print(df)
+
+        # saving results
+        group_name = sheet.cell(row=2, column=group_column).value
+        year_name = sheet.cell(row=1, column=group_column).value
+
+        groups_data_path = f'../../../data/groups_schedules/{year_name}'
+        cur_path = Path(__file__).parent
+        groups_data_path = (cur_path / groups_data_path).resolve()
+
+        if not os.path.exists(groups_data_path):
+            os.makedirs(groups_data_path)
+
+        df.to_csv((cur_path / groups_data_path / f'{group_name}.csv').resolve())
